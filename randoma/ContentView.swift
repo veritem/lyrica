f35f66e9b1e53790ec54e0ccde41d1aa9a52bf1e
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-05-24 15:21:13 +0200

    Initial Commit

---
+++ randoma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  randoma
+//
+//  Created by Makuza Mugabo Verite on 24/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ randoma/randomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomaApp.swift
+//  randoma
+//
+//  Created by Makuza Mugabo Verite on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct randomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
